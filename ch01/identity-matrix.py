b9843b2056fdbfc7bf10751f0b1bd0e3b6bf04b6
Manpreet <manpreet.singh@salesforce.com>
2017-06-11 22:53:46 +0530

    chapter 01 - nn

--- ch01/identity-matrix.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-identity = tf.eye(3, 3)
-
-with tf.Session() as sess:
-    print(sess.run(identity))
--- ch01/vector-norm.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-vector = tf.constant([[4,5,6]], dtype=tf.float32)
-
-eucNorm = tf.norm(vector, ord="euclidean")
-
-with tf.Session() as sess:
-    print(sess.run(eucNorm))
